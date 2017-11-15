321788f49f170b6ebb5135d2d18d6cb2d7690c3c
Raed <raedbh@gmail.com>
2017-11-15 11:54:27 +0100

    Configure dropwizard & graphite

---
+++ src/main/kotlin/com/example/demo/GraphiteConfig.kt
@@ -0,0 +1,33 @@
+package com.example.demo
+
+import com.codahale.metrics.MetricFilter
+import com.codahale.metrics.MetricRegistry
+import com.codahale.metrics.graphite.Graphite
+import com.codahale.metrics.graphite.GraphiteReporter
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Configuration
+import java.net.InetSocketAddress
+import java.util.concurrent.TimeUnit
+import javax.annotation.PostConstruct
+
+
+@Configuration
+class GraphiteConfig {
+
+    @Autowired
+    private val registry: MetricRegistry? = null
+
+    @PostConstruct
+    fun initialize() {
+        val graphite = Graphite(InetSocketAddress("localhost", 2003))
+        val reporter = GraphiteReporter.forRegistry(registry)
+                .prefixedWith("demo")
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .filter(MetricFilter.ALL)
+                .build(graphite)
+        reporter.start(1, TimeUnit.MINUTES)
+
+    }
+
+}
