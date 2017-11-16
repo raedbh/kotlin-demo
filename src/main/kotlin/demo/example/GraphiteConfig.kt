d226998df1a67a96133cce2e6dd7552c0b13cb17
Raed <raedbh@gmail.com>
2017-11-16 13:07:39 +0100

    Externalize properties

--- src/main/kotlin/demo/example/GraphiteConfig.kt
+++ src/main/kotlin/demo/example/GraphiteConfig.kt
@@ -5,6 +5,7 @@ import com.codahale.metrics.MetricRegistry
 import com.codahale.metrics.graphite.Graphite
 import com.codahale.metrics.graphite.GraphiteReporter
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import java.net.InetSocketAddress
 import java.util.concurrent.TimeUnit
@@ -17,17 +18,25 @@ class GraphiteConfig {
     @Autowired
     lateinit var registry: MetricRegistry
 
+    @Value("\${graphite.carbon.host}")
+    lateinit var host: String
+
+    @Value("\${graphite.carbon.port}")
+    var port: Int = 0
+
+    @Value("\${graphite.time-between-polls}")
+    var period: Long = 0
+
     @PostConstruct
     fun initialize() {
-        val graphite = Graphite(InetSocketAddress("localhost", 2003))
+        val graphite = Graphite(InetSocketAddress(host, port))
         val reporter = GraphiteReporter.forRegistry(registry)
                 .prefixedWith("kotlin-demo")
                 .convertRatesTo(TimeUnit.SECONDS)
                 .convertDurationsTo(TimeUnit.MILLISECONDS)
                 .filter(MetricFilter.ALL)
                 .build(graphite)
-        reporter.start(1, TimeUnit.MINUTES)
-
+        reporter.start(period, TimeUnit.MINUTES)
     }
 
 }
